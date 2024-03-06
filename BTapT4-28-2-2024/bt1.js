4e9fe8bbc5b99d9e22f1b6fcb430d47b6f6cf7d4
Phu21CNTT2 <danganhphu2707@gmail.com>
2024-03-06 09:46:44 +0700

    baitap

---
+++ BTapT4-28-2-2024/bt1.js
@@ -0,0 +1,18 @@
+var books = [
+    {
+        tacGia : 'DangAnhPhu',
+        tieuDe : 'Books1',
+        namXuatBan :'2024',
+        trangThai : true
+    },
+    {
+        tacGia : 'DangAnhPhu',
+        tieuDe : 'Books1',
+        namXuatBan :'2024',
+        trangThai : true
+    }
+]
+books.forEach((item)=>{
+   console.log(`tacGia : ${item.tacGia} tieuDe : ${item.tieuDe}  namXuatBan : ${item.namXuatBan} TrangThai : ${item.trangThai}`)
+})
+
